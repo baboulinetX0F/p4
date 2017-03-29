529b6c1200b62e55bd1230137d448736e72d6425
Antoine <antoine.tristani.123@gmail.com>
2017-03-29 19:36:39 +0200

    julien tu sens le saykse

--- p4/Button.h
+++ p4/Button.h
@@ -17,27 +17,27 @@ public:
 	void SetText(std::string newText);
 	void SetActionOnClick(void(GameManager::*ptr)(void));
 
-    bool isHovered() const;
+	bool isHovered() const;
 	bool isClicked() const;
 
-    virtual void Draw(Renderer* renderer);
-    virtual void HandleEvents(SDL_Event e);
+	virtual void Draw(Renderer* renderer);
+	virtual void HandleEvents(SDL_Event e);
 
 protected:
-    // TODO : Renommer les variables et ajouter Getter isClicked
-    bool m_hovered = false;
-    bool m_clicked = false;
+	// TODO : Renommer les variables et ajouter Getter isClicked
+	bool m_hovered = false;
+	bool m_clicked = false;
 
-    std::string m_text = BUTTON_TEXT_DEFAULT;  
+	std::string m_text = BUTTON_TEXT_DEFAULT;  
 
 	bool m_customTexture = false;
-    SDL_Texture* m_buttonTexture = nullptr;
-    SDL_Texture* m_buttonHoveredTexture = nullptr;
-    SDL_Texture* m_buttonClickedTexture = nullptr;
+	SDL_Texture* m_buttonTexture = nullptr;
+	SDL_Texture* m_buttonHoveredTexture = nullptr;
+	SDL_Texture* m_buttonClickedTexture = nullptr;
 
-    SDL_Texture* m_textTexture = nullptr;
+	SDL_Texture* m_textTexture = nullptr;
 
-    virtual void OnClick();
+	virtual void OnClick();
 
 private:
 	SDL_Rect m_textRect;
--- p4/Game.cpp
+++ p4/Game.cpp
@@ -16,45 +16,73 @@ Game::~Game()
 
 void Game::GameLoop()
 {
-    UISetup();
-    while (!m_quit)
-    {
-        PollEvents();
-        m_renderer->Clear();
-        m_manager->Update();
+	UISetup();
+	while (!m_quit)
+	{
+		PollEvents();
+		m_renderer->Clear();
+		m_manager->Update();
 
-        for (unsigned int i = 0; i < m_ui.size(); i++)
-            m_ui.at(i)->Draw(m_renderer);
+		for (unsigned int i = 0; i < m_ui.size(); i++)
+			m_ui.at(i)->Draw(m_renderer);
 
-        m_renderer->Render();
-    }
+		m_renderer->Render();
+	}
 }
 
 void Game::PollEvents()
 {
-    SDL_Event e;
+	SDL_Event e;
 	while (SDL_PollEvent(&e) != 0)
 	{
 		if (e.type == SDL_QUIT)
 			m_quit = true;
 		else
 		{
-            for (unsigned int i = 0; i < m_ui.size(); i++)
-                m_ui.at(i)->HandleEvents(e);
+			for (unsigned int i = 0; i < m_ui.size(); i++)
+				m_ui.at(i)->HandleEvents(e);
 		}
 	}
 }
 
 void Game::UISetup()
 {
-    Button* button = new Button();
-    button->SetPos(1280-180, 15);
-    button->SetSize(180, 60);
-	button->SetText("Restart");
-	button->SetActionOnClick(&GameManager::Restart);
-    m_ui.push_back(button); 
-		
-    PGrid* grid = new PGrid(m_manager);
+	Button* restart = new Button();
+	restart->SetPos(1280-180, 15);
+	restart->SetSize(180, 60);
+	restart->SetText("Restart");
+	restart->SetActionOnClick(&GameManager::Restart);
+	m_ui.push_back(restart);
+
+	Button* Diff1 = new Button();
+	Diff1->SetPos(1280 - 180, 90);
+	Diff1->SetSize(180, 60);
+	Diff1->SetText("Facile");
+	Diff1->SetActionOnClick(&GameManager::DifficulteUn);
+	m_ui.push_back(Diff1);
+
+	Button* Diff2 = new Button();
+	Diff2->SetPos(1280 - 180, 165);
+	Diff2->SetSize(180, 60);
+	Diff2->SetText("Moyen");
+	Diff2->SetActionOnClick(&GameManager::DifficulteDeux);
+	m_ui.push_back(Diff2);
+
+	Button* Diff3 = new Button();
+	Diff3->SetPos(1280 - 180, 240);
+	Diff3->SetSize(180, 60);
+	Diff3->SetText("Difficile");
+	Diff3->SetActionOnClick(&GameManager::DifficulteTrois);
+	m_ui.push_back(Diff3);
+
+	Button* Diff4 = new Button();
+	Diff4->SetPos(1280 - 180, 315);
+	Diff4->SetSize(180, 60);
+	Diff4->SetText("Hardcore");
+	Diff4->SetActionOnClick(&GameManager::DifficulteQuatre);
+	m_ui.push_back(Diff4);
+
+	PGrid* grid = new PGrid(m_manager);
 	grid->SetPos(300,50);
 	m_ui.push_back(grid);
 }
